e587fa2c6f9278e390bceb8b10e5013c461a5b1e
TAUBIRA Mehdi <mtaubira@iceb.com>
2025-04-10 16:57:34 +0200

    ajout show dans table

--- resources/js/Pages/TableDetails.vue
+++ resources/js/Pages/TableDetails.vue
@@ -94,6 +94,9 @@
                       <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                         Range value
                       </th>
+                      <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Show
+                      </th>
                     </tr>
                   </thead>
                   <tbody class="bg-white divide-y divide-gray-200">
@@ -224,6 +227,15 @@
                             </button>
                           </div>
                         </div>
+                      </td>
+                      <td class="px-4 py-3 text-sm">
+                          <!-- <SecondaryButton @click="openModalView(shop)"> -->
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="blue" class="size-6">
+                              <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                              <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                            </svg>
+
+                          <!-- </SecondaryButton> -->
                       </td>
                     </tr>
                   </tbody>
