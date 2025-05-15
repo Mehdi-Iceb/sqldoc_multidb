9a30f9edf38ec7be0d3debd698610fe6ef21a76a
TAUBIRA Mehdi <mtaubira@iceb.com>
2025-05-15 16:56:55 +0200

    ajout controller

---
+++ app/Http/Controllers/ReleaseController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Release;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Inertia\Inertia;
+
+
+class ReleaseController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function create()
+    {
+        
+    }
+}
