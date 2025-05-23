33d1368f4ea4161b4dc37773ca52ac62ed8c43b5
TAUBIRA Mehdi <mtaubira@iceb.com>
2025-05-23 13:08:38 +0200

    modif navigation if admin

--- resources/js/Layouts/Navigation.vue
+++ resources/js/Layouts/Navigation.vue
@@ -37,6 +37,19 @@
             administration
           </NavLink>
         </li>
+
+        <li v-if="$page.props.auth.user.role === 'admin'" class="relative px-6 py-3">
+          <NavLink :href="route('releases.index')" :active="route().current('releases.index')">
+            <template #icon>
+              <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round"
+                  stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                <path
+                  d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2"></path>
+              </svg>
+            </template>
+            Releases
+          </NavLink>
+        </li>
         
         <li class="relative px-6 py-3">
           <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
@@ -59,19 +72,6 @@
               </svg>
             </template>
             My project
-          </NavLink>
-        </li>
-
-        <li class="relative px-6 py-3">
-          <NavLink :href="route('releases.index')" :active="route().current('releases.index')">
-            <template #icon>
-              <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round"
-                  stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                <path
-                  d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2"></path>
-              </svg>
-            </template>
-            Releases
           </NavLink>
         </li>
 
