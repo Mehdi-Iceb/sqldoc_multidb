e44ccff3041fe1bd61ad2297adc65425ef54847f
TAUBIRA Mehdi <mtaubira@iceb.com>
2025-10-03 10:15:00 +0200

    correction trigger

--- resources/js/Pages/TriggerDetails.vue
+++ resources/js/Pages/TriggerDetails.vue
@@ -30,7 +30,12 @@
             <div class="bg-white rounded-lg shadow-sm overflow-hidden mb-6">
               <div class="px-6 py-4 border-b border-gray-100 bg-gray-50">
                 <div class="flex justify-between items-center">
-                  <h3 class="text-lg font-medium text-gray-900">Description</h3>
+                  <h3 class="text-lg font-medium text-gray-900 flex items-center gap-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+                    </svg>
+                    Description
+                  </h3>
                   <button 
                     @click="saveDescription" 
                     class="px-3 py-1 bg-blue-600 text-white text-sm rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -53,7 +58,12 @@
             <!-- Informations générales -->
             <div class="bg-white rounded-lg shadow-sm overflow-hidden mb-6">
               <div class="px-6 py-4 border-b border-gray-100 bg-gray-50">
-                <h3 class="text-lg font-medium text-gray-900">General information</h3>
+                <h3 class="text-lg font-medium text-gray-900 flex items-center gap-1">
+                  <svg class="h-5 w-5 text-gray-500 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
+                </svg>
+                  Information
+                </h3>
               </div>
               <div class="p-6 grid grid-cols-2 gap-6">
                 <div>
@@ -107,7 +117,12 @@
             <!-- Définition SQL -->
             <div class="bg-white rounded-lg shadow-sm overflow-hidden mb-6">
               <div class="px-6 py-4 border-b border-gray-100 bg-gray-50">
-                <h3 class="text-lg font-medium text-gray-900">Definition SQL</h3>
+                <h3 class="text-lg font-medium text-gray-900 flex items-center gap-1">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25" />
+                  </svg>
+                  Definition SQL
+                </h3>
               </div>
               <div class="p-6">
                 <pre v-if="triggerDetails.definition" 
