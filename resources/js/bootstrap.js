bf7932c2b72cb8c4b7089ffa9e2a61a540da6f3d
TAUBIRA Mehdi <mtaubira@iceb.com>
2025-11-24 12:31:55 +0100

    modif bootstrap.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,14 +2,30 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-const token = document.head.querySelector('meta[name="csrf-token"]');
-
-if (token) {
-    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
-} else {
-    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
-}
-
-
 window.axios.defaults.withCredentials = true;
+
+// Intercepteur pour s'assurer que le token CSRF est toujours à jour
+axios.interceptors.request.use(config => {
+    const token = document.head.querySelector('meta[name="csrf-token"]');
+    
+    if (token) {
+        config.headers['X-CSRF-TOKEN'] = token.content;
+    }
+    
+    return config;
+}, error => {
+    return Promise.reject(error);
+});
+
+// Gestion automatique de l'erreur 419
+axios.interceptors.response.use(
+    response => response,
+    error => {
+        if (error.response?.status === 419) {
+            console.error('CSRF token mismatch - reloading page...');
+            // Recharger la page pour obtenir un nouveau token
+            window.location.reload();
+        }
+        return Promise.reject(error);
+    }
+);
