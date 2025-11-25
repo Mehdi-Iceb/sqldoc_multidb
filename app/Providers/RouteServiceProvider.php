c82a01afa2163800d93682185bc7b86cdea84eff
TAUBIRA Mehdi <mtaubira@iceb.com>
2025-11-25 09:31:40 +0100

    modif routeserviceprovider

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Stancl\Tenancy\Middleware\InitializeTenancyByDomain;
+use Stancl\Tenancy\Middleware\PreventAccessFromCentralDomains;
 
 class RouteServiceProvider extends ServiceProvider
 {
@@ -16,10 +18,12 @@ class RouteServiceProvider extends ServiceProvider
     
     protected function mapTenantRoutes(): void
     {
-        Route::middleware([
-            \Illuminate\Cookie\Middleware\EncryptCookies::class,
-            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            \App\Http\Middleware\InitializeTenantForced::class,
+        Route::middleware(['web',
+            InitializeTenancyByDomain::class,
+            PreventAccessFromCentralDomains::class,
+            //\Illuminate\Cookie\Middleware\EncryptCookies::class,
+            //\Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+           // \App\Http\Middleware\InitializeTenantForced::class,
             \Illuminate\Session\Middleware\StartSession::class,
         ])
         ->group(base_path('routes/tenant.php'));
