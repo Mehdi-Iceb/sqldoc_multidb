ea7542fd11b6e32e075dd3ad80aed2164ccec4ef
TAUBIRA Mehdi <mtaubira@iceb.com>
2025-11-25 12:42:52 +0100

    modif

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,7 +8,10 @@ use Stancl\Tenancy\Middleware\PreventAccessFromCentralDomains;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-         web: __DIR__.'/../routes/web.php',
+        web: [
+        __DIR__.'/../routes/web.php',
+        __DIR__.'/../routes/tenant.php', 
+        ],
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
