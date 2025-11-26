16e929596c57279c0a3b144bf8e51f391c599b61
TAUBIRA Mehdi <mtaubira@iceb.com>
2025-11-26 11:43:22 +0100

    modif ajout message port

--- resources/js/Pages/Projects/Connect.vue
+++ resources/js/Pages/Projects/Connect.vue
@@ -33,13 +33,13 @@
                                 <h3 class="text-lg font-medium text-gray-900">Login information</h3>
                                 <p class="text-sm text-gray-600">Database type: {{ getDbTypeName(project.db_type) }}</p>
 
-                                <div class="flex items-start mt-1 mb-2 text-sm text-blue-600 bg-blue-50 p-2 rounded-md">
+                                <div class="inline-flex items-start mt-1 mb-2 text-sm text-red-600 bg-blue-50 p-2 rounded-md">
                                     <svg class="w-4 h-4 mr-2 flex-shrink-0 mt-0.5" fill="currentColor" viewBox="0 0 20 20">
                                         <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"/>
                                     </svg>
                                     <span>Make sure the port is accessible and not blocked by a firewall.</span>
                                 </div>
-                                
+
                             </div>
                             <Link
                                 :href="route('projects.index')"
